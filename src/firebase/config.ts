dd51d70cf01e4ac04f09d45ca2822c2084aeecfd
Fernando Alvarez <fernandoalvarez@secocut.com>
2025-11-06 13:00:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1546170521-5d9b0",
+  "appId": "1:390176636803:web:ea4652902e1c246092c0b6",
+  "apiKey": "AIzaSyCHUJ52y_kSw7urqOxuj7aC-dhusJ6xas4",
+  "authDomain": "studio-1546170521-5d9b0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "390176636803"
+};
